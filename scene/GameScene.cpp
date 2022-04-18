18bdee8fcb2e52009275ae5d4056fce1bf4ab74d
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-04-18 09:58:20 +0900

    100個のオブジェクトの角度と座標を、乱数でランダムに設定し描画する

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -19,16 +19,11 @@ void GameScene::Initialize() {
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
 
+
 	//ファイル名を指定して読み込む
 	textureHandle_ = TextureManager::Load("mario.jpg");
 	//3Dモデルの生成
 	model_ = Model::Create();
-
-
-	//X,Y,Z方向のスケーリングを設定
-	worldTransform_.scale_ = { 5.0f, 5.0f, 5.0f };
-	//X,Y,Z軸周りの回転軸を設定
-	worldTransform_.rotation_ = { XM_PI / 4.0f, XM_PI / 4.0f, 0.0f };
 
 	//乱数シード生成器
 	std::random_device seed_gen;
@@ -37,23 +32,30 @@ void GameScene::Initialize() {
 	//乱数範囲(回転角用)
 	std::uniform_real_distribution<float> rotDist(0.0f, XM_2PI);
 	//乱数範囲(座標用)
-	std::uniform_real_distribution<float> rotDist(10.0f, 10.0f);
+	std::uniform_real_distribution<float> posDist(-10.0f, 10.0f);
 
+
+	for (size_t i = 0; i < _countof(worldTransform_); i++)
+	{
+	//X,Y,Z方向のスケーリングを設定
+	worldTransform_[i].scale_ = { 1.0f, 1.0f, 1.0f };
+	//X,Y,Z軸周りの回転角を設定
+	worldTransform_[i].rotation_ = {rotDist(engine),rotDist(engine), rotDist(engine)};
 	//X,Y,Z方向の平行移動を設定
-	worldTransform_.translation_ = { 10.0f, 10.0f, 10.0f };
-
+	worldTransform_[i].translation_ = { posDist(engine), posDist(engine), posDist(engine) };
 
 	//ワールドトランスフォームの初期化
-	worldTransform_.Initialize();
+	worldTransform_[i].Initialize();
+
+	}
+
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
-
-	
 }
 
 void GameScene::Update() {
 
-	debugText_->SetPos(50, 50);
+	/*debugText_->SetPos(50, 50);
 	debugText_->Printf("transform:(%f,%f,%f)",
 		worldTransform_.translation_.x, worldTransform_.translation_.y, worldTransform_.translation_.z);
 
@@ -63,7 +65,7 @@ void GameScene::Update() {
 
 	debugText_->SetPos(50, 90);
 	debugText_->Printf("scale:(%f,%f,%f)",
-		worldTransform_.scale_.x, worldTransform_.scale_.y, worldTransform_.scale_.z);
+		worldTransform_.scale_.x, worldTransform_.scale_.y, worldTransform_.scale_.z);*/
 
 }
 
@@ -93,8 +95,10 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-	///
-	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+	for (size_t i = 0; i < _countof(worldTransform_); i++)
+	{
+	model_->Draw(worldTransform_[i], viewProjection_, textureHandle_);
+	}
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
